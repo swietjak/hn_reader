4d32c79ae658ec8d3699e1112dea2047e5d2b866
swietjak <56366879+swietjak@users.noreply.github.com>
2020-09-09 14:12:53 +0200

    Updates

---
+++ precache-manifest.4b39cabee8816a871521bca4ef062b4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "553ef7f435f34bd19b735a3b9dd340ce",
+    "url": "/hn_reader/index.html"
+  },
+  {
+    "revision": "1f53156acbfb8173e278",
+    "url": "/hn_reader/static/css/main.bc499902.chunk.css"
+  },
+  {
+    "revision": "9da55d5a6a21763338d9",
+    "url": "/hn_reader/static/js/2.64fc9238.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hn_reader/static/js/2.64fc9238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f53156acbfb8173e278",
+    "url": "/hn_reader/static/js/main.9747ad39.chunk.js"
+  },
+  {
+    "revision": "4e13bece23f3d7091f9d",
+    "url": "/hn_reader/static/js/runtime-main.3fe160fa.js"
+  }
+]);
